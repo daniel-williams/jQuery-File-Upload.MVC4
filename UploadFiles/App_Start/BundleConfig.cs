abe419204c30a00450a0cedd3b6a5d35c3a7674f
Daniel Williams <mail@danielwilliams.us>
2013-01-07 11:43:02 -0800

    Moved javascript and css into bundles.

--- UploadFiles/App_Start/BundleConfig.cs
+++ UploadFiles/App_Start/BundleConfig.cs
@@ -8,36 +8,42 @@ namespace UploadFiles
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/styles/Bootstrap/css")
+                .Include("~/styles/Bootstrap/bootstrap.css")
+                .Include("~/styles/Bootstrap/bootstrap-responsive.css")
+                );
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new StyleBundle("~/styles/css")
+                .Include("~/styles/site.css")
+                );
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new StyleBundle("~/styles/FileUpload/css")
+                .Include("~/styles/FileUpload/jquery.fileupload-ui.css")
+                );
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/scripts/js")
+                .Include("~/scripts/jquery-{version}.js")
+                .Include("~/scripts/jquery-ui-{version}.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/bootstrap.js")
+                );
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new ScriptBundle("~/scripts/modernizr")
+                .Include("~/scripts/modernizr-*")
+                );
+
+            bundles.Add(new ScriptBundle("~/scripts/FileUpload/js")
+                .Include("~/scripts/FileUpload/tmpl.js")
+                .Include("~/scripts/FileUpload/load-image.js")
+                .Include("~/scripts/FileUpload/canvas-to-blob.js")
+                .Include("~/scripts/FileUpload/jquery.iframe-transport.js")
+                .Include("~/scripts/FileUpload/jquery.fileupload.js")
+                .Include("~/scripts/FileUpload/jquery.fileupload-ip.js")
+                .Include("~/scripts/FileUpload/jquery.fileupload-ui.js")
+                .Include("~/scripts/FileUpload/locale.js")
+                .Include("~/scripts/FileUpload/main.js")
+                );
         }
     }
 }
